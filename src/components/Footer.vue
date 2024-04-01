d38069db46ba3ef077deec74d8b510160043b6a4
Dereden399 <denisk399@gmail.com>
2024-04-01 12:16:11 +0300

    Better Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="w-full bg-accent-200">
-    <p>Footer</p>
+  <div class="flex w-full flex-col items-center text-slate-950">
+    <div
+      class="my-16 flex flex-col items-center border-t-2 border-blue-300 py-8"
+    >
+      <p>Designed and built by Denis</p>
+      <blockquote class="mt-8 italic">
+        <p>"Quality is not an act, it is a habit."</p>
+      </blockquote>
+      <cite class="mt-2 text-sm text-slate-700">Aristotle</cite>
+    </div>
   </div>
 </template>
 
